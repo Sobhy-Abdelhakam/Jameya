0032e4249c82341433ea403556a2fd512682916c
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-01-13 10:36:39 +0200

    change launcher icon

--- app/src/main/java/dev/sobhy/jameya/MainActivity.kt
+++ app/src/main/java/dev/sobhy/jameya/MainActivity.kt
@@ -1,23 +1,16 @@
 package dev.sobhy.jameya
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.sobhy.jameya.navigation.AppNavHost
-import dev.sobhy.jameya.presentation.login.LoginScreen
 import dev.sobhy.jameya.ui.theme.JameyaTheme
 
 @AndroidEntryPoint
@@ -31,9 +24,6 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             JameyaTheme {
                 Surface {
-                    val url = BuildConfig.SUPABASE_URL
-                    val key = BuildConfig.SUPABASE_KEY
-                    Log.d("MainActivity", "onCreate: $url $key")
                     AppNavHost(navController = navController, startDestination = startDestination!!)
                 }
             }
