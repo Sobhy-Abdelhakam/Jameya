78d410c8c17faebbf91783b91afc6e2614f2d154
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-01-21 22:28:06 +0200

    convert timestamp to local date

--- app/src/main/java/dev/sobhy/jameya/data/mappers/UserMapper.kt
+++ app/src/main/java/dev/sobhy/jameya/data/mappers/UserMapper.kt
@@ -2,12 +2,21 @@ package dev.sobhy.jameya.data.mappers
 
 import dev.sobhy.jameya.data.dto.UserDto
 import dev.sobhy.jameya.domain.model.User
-import kotlinx.datetime.LocalDate
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun convertSupabaseTimestamp(timestamp: String): LocalDate {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    val localDateTime = LocalDateTime.parse(timestamp, formatter)
+    return localDateTime.atZone(ZoneId.systemDefault()).toLocalDate()
+}
 
 fun UserDto.toDomain(): User = User(
     id = id,
-    name = fullName,
+    name = fullName ?: "New User",
     image = image,
-    createdAt = LocalDate.parse(createdAt),
-    updatedAt = LocalDate.parse(updatedAt)
+    createdAt = convertSupabaseTimestamp(createdAt),
+    updatedAt = convertSupabaseTimestamp(updatedAt)
 )
